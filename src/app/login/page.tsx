7cb5974816407340b6b852712878d233ad812102
Franz Herrmann <Xignotic84@gmail.com>
2025-06-22 13:15:09 +0200

    chore: add redirect on login page if session exists

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,21 @@
+'use client'
 import { GitMerge } from 'lucide-react'
 
 import { LoginForm } from '@/components/auth/signInForm'
+import { useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 export default function LoginPage() {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      router.replace('/') // or wherever you want to redirect
+    }
+  }, [status, router])
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
