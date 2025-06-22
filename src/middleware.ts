382aa4390110aabf0c9691d38d3d6d0f9d7d261f
Franz Herrmann <Xignotic84@gmail.com>
2025-06-22 12:23:28 +0200

    chore: redirect on unauthorized session

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,12 @@
-export { auth as middleware } from '@/server/auth'
+import { auth } from '@/server/auth'
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== '/login') {
+    const newUrl = new URL('/login', req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+})
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
